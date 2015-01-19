e2530a5e7a0103b5e1b938221a0ee4af82abe69e
vadim demeshyn <vadimdemeshyn@MacBook-vadim.local>
2015-01-19 20:38:29 +0200

    run

--- maven-project/Maven/bin/src/test/java/glue/MyStepdefs.java
+++ maven-project/Maven/bin/src/test/java/glue/MyStepdefs.java
@@ -2,9 +2,8 @@ package glue;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,46 +18,41 @@ public class MyStepdefs extends Configurations {
 
     @Given("^I am on Google home page$")
     public void I_am_on_Google_home_page() throws Throwable {
-        //If wanna use chromedriver for testing in chrome. Can be also implemented in global configuration class.
-        // System.setProperty("webdriver.chrome.driver", "/chromedriver.exe");
+
          browser.get("https://www.google.com");
     }
 
-    @When("^I search 'google translate' text$")
-    public void I_search_google_translate_text() throws Throwable {
-        browser.findElement(By.id("gbqfq")).sendKeys("google translate");
+    @When("^I search \"([^\"]*)\" text$")
+    public void I_search_text(String arg1) throws Throwable {
+        browser.findElement(By.id("gbqfq")).sendKeys(arg1);
     }
 
-    @And("^I open result page in other browser's session$")
-    public void I_open_result_page_in_other_browser_s_session() throws Throwable {
+    @And("^Open result page that is linked to \"([^\"]*)\"$")
+    public void Open_result_page_that_is_linked_to(String arg1) throws Throwable {
         WebElement element1 = delay.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("id('rso')//li[2]/div/h3/a")));
+        element1.click();
+    }
 
-     // getting URL from needed Google response
-        String href = element1.getAttribute("href");
-        WebDriver browser = new FirefoxDriver() {
-        };
-        browser.get(href);
+    @And("^I Enter phrase \"([^\"]*)\" into textarea field$")
+    public void I_Enter_phrase_into_textarea_field(String arg1) throws Throwable {
+        WebElement elem = delay.until(ExpectedConditions.visibilityOfElementLocated(By.id("source")));
+        elem.sendKeys(arg1);
 
-     // switching to new browser window (new session, as requested)
-        for (String winHandle : browser.getWindowHandles()) {
-            browser.switchTo().window(winHandle);
+    }
 
-        }
-
-
-     //And I enter phrase 'luxoft test task' into text area field using it's id ,select right language to 'Polish' and click on 'Translate' button
-        WebElement elem = browser.findElement(By.id("source"));
-        elem.sendKeys("luxoft test task");
-
+    @And("^I Select right language to 'Polish' and click on 'Translate' button$")
+    public void I_Select_right_language_to_Polish_and_click_on_Translate_button() throws Throwable {
         browser.findElement(By.id("gt-tl-gms")).click();
-        browser.findElement(By.id(":47")).click();
+        browser.findElement(By.id("4с")).click();
+    }
 
-     //Implementing step - should ensure that result contains 'zadaniem testu'
 
+    @Then("^I should ensure that result contains \"([^\"]*)\" string$")
+    public void I_should_ensure_that_result_contains_string(String arg1) throws Throwable {
         String elemCheck = browser.findElement(By.id("result_box")).getText();
 
         Assert.assertTrue("Zadaniem testu", elemCheck.contains("Zadaniem testu"));
         browser.close();
-
     }
+
 }
--- maven-project/Maven/bin/src/test/resources/feature.feature
+++ maven-project/Maven/bin/src/test/resources/feature.feature
@@ -2,5 +2,8 @@ Feature: E2E Test
   Scenario: testing translation for new employee test
 
     Given I am on Google home page
-    When I search 'google translate' text
-    And I open result page in other browser's session
+    When I search "google translate" text
+    And Open result page that is linked to "translate.google.com"
+    And I Enter phrase "my test task" into textarea field
+    And I Select right language to 'Polish' and click on 'Translate' button
+    Then I should ensure that result contains "zadaniem testu" string

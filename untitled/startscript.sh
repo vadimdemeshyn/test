4551bdd5b59f56026ebccc6eba7692381efa594a
vadimdemeshyn <2810199128101991@rambler.ru>
2016-01-03 23:23:11 +0200

    Update startscript.sh

--- untitled/startscript.sh
+++ untitled/startscript.sh
@@ -4,8 +4,8 @@ killall -9 phantomjs
 
 cd ~/test/untitled
 
-java -jar ./selenium-server-standalone-2.40.0.jar -role hub &>~/test/untitled/error_selenium.log & disown
+java -jar ./selenium-server-standalone-2.40.0.jar -role hub & disown
 
-./phantomjs-1.9.7-linux-x86_64/bin/phantomjs --webdriver=5555 --webdriver-selenium-grid-hub=http://127.0.0.1:4444 &>~/test/untitled/error_phantom.log & disown
+./phantomjs-1.9.7-linux-x86_64/bin/phantomjs --webdriver=5555 --webdriver-selenium-grid-hub=http://127.0.0.1:4444 & disown
 
 mvn clean install -Dcucumber.options=" --tags @smoke" 
